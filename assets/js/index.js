6066c66454a40cc131024d9f83fdedc6d39bc11a
etrinhh <etrinhh@gmail.com>
2021-02-22 20:27:59 -0700

    js will ruin me because idk what to do

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,18 @@
 "use strict";
+// i hope this is right?? but images took too much room and it was cluttered
 import { images } from "./gallery";
 
 const gallery = document.querySelector(".gallery");
+
+images.forEach(function (image) {});
+
+/*
+
+this was my code from the regular html
+i gotta SOMEHOW turn my js into this ????
+              <figure class="gallery-item">
+                <img src="./Assets/Images/happy-gallery.jpg" alt="Happy dog" />
+                <figcaption>Livin my best life</figcaption>
+              </figure>
+        LMFAO fml idk wat do
+*/
