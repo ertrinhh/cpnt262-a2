9c1263c9a9cf20d98177f139d1917af6e1a6d277
etrinhh <etrinhh@gmail.com>
2021-02-22 20:06:28 -0700

    i dont rly know what im doing yet

---
+++ assets/js/gallery.js
@@ -0,0 +1,65 @@
+export const images = [
+{
+  id: 1,
+  title: "Banana",
+  description: "Dog with a banana toy"
+  width: 500,
+  credit: "Erica"
+},
+{
+  id: 2,
+  title: "bear",
+  description: "Dog in a bear costume"
+  width: 500,
+  credit: "Erica"
+},
+{
+  id: 3,
+  title: "Bow",
+  description: "Dapper dog with a bowtie"
+  width: 500,
+  credit: "Erica"
+},
+{
+  id: 4,
+  title: "Cow",
+  description: "Dog with his favorite toy cow"
+  width: 500,
+  credit: "Erica"
+},
+{
+  id: 5,
+  title: "Happy",
+  description: "A really happy dog"
+  width: 500,
+  credit: "Erica"
+},
+{
+  id: 6,
+  title: "scarf",
+  description: "A fashionable dog with a scarf"
+  width: 500,
+  credit: "Erica"
+},
+{
+  id: 7,
+  title: "Shapespeare",
+  description: "Dog with a collar thing that looks like Shakespeare"
+  width: 500,
+  credit: "Erica"
+},
+{
+  id: 8,
+  title: "Shoes",
+  description: "Dog wearing shoes"
+  width: 500,
+  credit: "Erica"
+},
+{
+  id: 9,
+  title: "Sleeping",
+  description: "Dog taking a bit of a snooze"
+  width: 500,
+  credit: "Erica"
+}
+];
---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+"use strict";
+import { images } from "./gallery";
+
+const gallery = document.querySelector(".gallery");
