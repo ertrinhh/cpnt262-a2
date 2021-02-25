162b87b76afc6c8dbd25e9af1d759a7ca35ed1e5
etrinhh <etrinhh@gmail.com>
2021-02-25 15:39:01 -0700

    holy FFFFff it WORKSSSSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,83 @@
 "use strict";
-// i hope this is right?? but images took too much room and it was cluttered
-import { images } from "./gallery";
+
+const pictures = [
+  {
+    id: 1,
+    title: "Banana",
+    description: "Dog with a banana toy",
+    width: 500,
+    path: "banana-gallery",
+  },
+  {
+    id: 2,
+    title: "bear",
+    description: "Dog in a bear costume",
+    width: 500,
+    path: "bear-gallery",
+  },
+  {
+    id: 3,
+    title: "Bow",
+    description: "Dapper dog with a bowtie",
+    width: 500,
+    path: "bow-gallery",
+  },
+  {
+    id: 4,
+    title: "Cow",
+    description: "Dog with his favorite toy cow",
+    width: 500,
+    path: "congee-cow",
+  },
+  {
+    id: 5,
+    title: "Happy",
+    description: "A really happy dog",
+    width: 500,
+    path: "happy-gallery",
+  },
+  {
+    id: 6,
+    title: "scarf",
+    description: "A fashionable dog with a scarf",
+    width: 500,
+    path: "scarf-gallery",
+  },
+  {
+    id: 7,
+    title: "Shapespeare",
+    description: "Dog with a collar thing that looks like Shakespeare",
+    width: 500,
+    path: "shakesp-gallery",
+  },
+  {
+    id: 8,
+    title: "Shoes",
+    description: "Dog wearing shoes",
+    width: 500,
+    path: "shoes-gallery",
+  },
+  {
+    id: 9,
+    title: "Sleeping",
+    description: "Dog taking a bit of a snooze",
+    width: 400,
+    path: "sleeping-gallery",
+  },
+];
 
 const gallery = document.querySelector(".gallery");
-let output = '',
+let output = "";
 
-images.forEach(function (image) {
-  output += `<figure class="gallery-item>
-  <img src="./images/${images.path}.jpg alt="${images.description}" width="${images.width}"/>`
+pictures.forEach(function (image) {
+  output += `<figure class="gallery-item">
+  <img src="./assets/images/${image.path}.jpg" alt="${image.title}"/>
+  <figcaption>${image.description}</figcaption>
+  </figure>`;
+
+  gallery.innerHTML = output;
 });
 
-document.querySelector('.gallery').innerHTML = output;
 /*
 
 this was my code from the regular html
